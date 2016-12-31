c4d5e86727d0803778bfcf1fabb2b35cbee2f2f9
Scott Wittrock <scott@thirdstreetgarage.com>
2016-12-31 13:59:15 -0500

    Initial Commit

--- templates/new-game-form.php
+++ templates/new-game-form.php
@@ -1,7 +1,7 @@
 <script id="newGameForm" type="text/html" class="handlebars-template">
     <div id="newGameForm" class="col-xs-12 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
         <div class="col-xs-12">
-            <h4><i class="fa fa-play"></i> Start a New Game of Seven Testing</h4>
+            <h4><i class="fa fa-play"></i> Start a New Game of Seven</h4>
         </div>
         <?php for($i=0; $i<7; $i++) { ?>
             <div class="col-xs-12 row-buffered">
